a2765f4e32d6ae4dce95a46818c29f5d6826e031
basantasharma <gyawalibasanta0@gmail.com>
2023-03-09 13:27:35 +0545

    changes on web socket last error remain

--- chatProject/chatApp/consumers.py
+++ chatProject/chatApp/consumers.py
@@ -1,7 +1,9 @@
 from channels.consumer import AsyncConsumer, SyncConsumer
 from channels.exceptions import StopConsumer
 import json
-from channels.generic.websocket import WebsocketConsumer, AsyncJsonWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncJsonWebsocketConsumer, AsyncWebsocketConsumer
+#from channels.generic.websocket import AsyncWebsocketConsumer
+#from asgiref.sync import async_to_sync
 #from channels.generic.websocket import AsyncJsonWebsocketConsumer
 # class mySyncConsumer(SyncConsumer):
 #     def websocket_connect(self, event):
@@ -43,15 +45,33 @@ class myAsyncConsumer(AsyncConsumer):
         raise StopConsumer()
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = "chat_%s" % self.room_name
 
-    def disconnect(self, close_code):
-        pass
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-    def receive(self, text_data):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
--- chatProject/chatProject/settings.py
+++ chatProject/chatProject/settings.py
@@ -73,8 +73,16 @@ TEMPLATES = [
 ]
 
 #WSGI_APPLICATION = "chatProject.wsgi.application"
+# Channels
 ASGI_APPLICATION = "chatProject.asgi.application"
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 
 # Database
